0bde5798c79f6fd5b952a4d7c7af87d633b527a1
xingwei07 <1597049692@qq.com>
2021-11-27 22:11:12 +0800

    增加history模式与hash模式

---
+++ 43_src_history模式与hash模式/pages/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <h2>我是About的内容</h2>
+</template>
+
+<script>
+  export default {
+    name: 'About',
+    mounted(){
+      // console.log('About被挂载了')
+      window.aboutRoute = this.$route
+      window.aboutRouter = this.$router
+    },
+    // beforeDestroy(){
+    //   console.log('About被销毁了')
+    // },
+    beforeRouteEnter (to, from, next) {
+      console.log('about——beforeRouteEnter',to,from)
+      if(to.meta.isAuth){
+        if(localStorage.getItem('school') == 'atguigu') {
+          next()
+        } else {
+          alert('无权限！')
+        }
+      }
+    },
+    beforeRouteLeave (to, from, next) {
+      console.log('About——beofreRouteLeave',to,from)
+      next()
+    }
+  }
+</script>
