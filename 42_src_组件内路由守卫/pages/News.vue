752109f4a9f13966bab2e1672914d2469eecd13e
xingwei07 <1597049692@qq.com>
2021-11-27 22:10:39 +0800

    增加组件内路由守卫

---
+++ 42_src_组件内路由守卫/pages/News.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <ul>
+      <li :style="{opacity}">欢迎学习vue</li>
+      <li>news001 <input type="text"></li>
+      <li>news002 <input type="text"></li>
+      <li>news003 <input type="text"></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'News',
+    data() {
+      return {
+        opacity: 1
+      }
+    },
+    beforeDestroy(){
+      console.log('News组件即将被销毁了')
+    },
+    activated(){
+      this.timer = setInterval(()=>{
+        console.log('@')
+        this.opacity -= 0.01
+        if(this.opacity < 0) this.opacity = 1 
+      },16)
+      console.log('News组件被激活了')
+    },
+    deactivated(){
+      clearInterval(this.timer)
+      console.log('News组件失活了')
+    }
+  }
+</script>
